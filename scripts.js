681b91efa90395782a43244d276961f4e036306c
KevinoCodes <58299737+KevinoCodes@users.noreply.github.com>
2022-07-30 18:19:38 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function Multiply() {
+    var Num1 = document.getElementById('number1').value;
+    var Num2 = document.getElementById('number2').value;
+    console.log(Num1)
+    console.log(Num2)
+    var answer = Num1 * Num2
+    document.getElementById('answertext').innerHTML = answer
+}
+
+function javascript1() {
+    console.log(hello)
+}
