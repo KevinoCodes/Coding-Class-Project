01582fc3db1b7ac4ffa70c8f8cb0b93a19b36099
KevinoCodes <58299737+KevinoCodes@users.noreply.github.com>
2022-08-05 19:46:14 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,53 @@
-function Multiply() {
-    var Num1 = document.getElementById('number1').value;
-    var Num2 = document.getElementById('number2').value;
-    console.log(Num1)
-    console.log(Num2)
-    var answer = Num1 * Num2
-    document.getElementById('answertext').innerHTML = answer
+function calcMultiply() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    console.log(num1*num2)
+    document.getElementById("answer").innerHTML = num1*num2
 }
 
-function javascript1() {
-    console.log(hello)
+function calcAddition() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    document.getElementById("answer").innerHTML = parseInt(num1) + parseInt(num2)
+    
+}
+
+function calcSubtraction() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value0
+
+
+
+    
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    document.getElementById("answer").innerHTML = parseInt(num1) - parseInt(num2)
+
+}
+
+function calcDivision() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    document.getElementById("answer").innerHTML = parseInt(num1) / parseInt(num2)
+
+}
+
+function calcExponent() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    document.getElementById("answer").innerHTML = parseInt(num1) ** parseInt(num2)
+
 }
